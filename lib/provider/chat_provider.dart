86ad66eed822b2886f087342a41522809f3788ab
HOANG Trung Thong <thoang5@mantu.com>
2024-04-25 20:30:21 +0700

    join event

--- lib/provider/chat_provider.dart
+++ lib/provider/chat_provider.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: avoid_print
 
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:sharing_cafe/constants.dart';
